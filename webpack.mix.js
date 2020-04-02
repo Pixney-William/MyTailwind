78e99635da832dd85aabd459caa9f68683f665fa
William Åström <william@pixney.com>
2020-04-02 12:30:19 +0200

    Added statamic

--- webpack.mix.js
+++ webpack.mix.js
@@ -64,7 +64,15 @@ if (mix.inProduction()) {
 
 
 
+/*
+ |--------------------------------------------------------------------------
+ | Statamic Control Panel Assets
+ |--------------------------------------------------------------------------
+ |
+ | Feel free to add your own JS or CSS to the Statamic Control Panel.
+ | https://statamic.dev/extending/control-panel#adding-css-and-js-assets
+ |
+ */
 
-
-
-
+// mix.js('resources/js/cp.js', 'public/vendor/app/js')
+//    .sass('resources/sass/cp.scss', 'public/vendor/app/css');
